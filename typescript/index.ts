38dd3e9de2779fc1d08a8e6e8a253e374a300191
Becerra Borges, Eduardo Yorman <eduardo.ybb.2018@gmail.com>
2024-06-19 08:07:42 -0500

    minor changes

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,30 +1,40 @@
-// Interfaces
+// Enumeración para los colores
 enum Color {
-  Rojo = 'Rojo',
-  Verde = 'Verde',
+  Rojo = "Rojo",
+  Verde = "Verde",
 }
 
+// Interfaz para los rectángulos
 interface Rectangulo {
   ancho: number;
   alto: number;
   color?: Color;
 }
 
+// Creación de un rectángulo con las propiedades ancho y alto
 let rect: Rectangulo = {
   ancho: 4,
   alto: 6,
-  // color: Color.Rojo,
+  // color: Color.Rojo, // Descomentar si se quiere asignar un color
 };
 
+// Función para calcular el área de un rectángulo
 function area(r: Rectangulo): number {
   return r.alto * r.ancho;
 }
 
+// Cálculo del área del rectángulo
 const areaRect = area(rect);
-console.log(areaRect);
+console.log(areaRect); // Salida: 24
 
-rect.toString = function() {
-  return this.color ? `Un rectangulo ${this.color}` : `Un rectangulo`;
-};
+// Añadiendo el método toString a la instancia rect
+rect = Object.assign(rect, {
+  toString: function () {
+    return this.color
+      ? `Un rectángulo ${this.color}`
+      : `Un rectángulo sin color`;
+  },
+});
 
-console.log(rect.toString());
+// Llamada al método toString y salida en consola
+console.log(rect.toString()); // Salida: Un rectángulo sin color
